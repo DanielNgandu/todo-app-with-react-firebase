108a7a8ddd9f39ece140cfb55ce907a68ac0b847
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-10-01 10:46:42 +0200

    Added material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//declare constants
+    const [todos, setTodos] = useState(['Read the bible', 'Sweep the room', 'Go to the market', 'Exercise']);
+    const [input, setInput] = useState('');
+
+//submit method
+    const addTodo = (event) => {
+        //fire up logic here
+        //prevent refresh after submit
+        event.preventDefault()
+        setTodos([...todos, input])//add new input value to array
+        setInput('')//reset form to empty
+    }
+
+    return (
+        <div className="App">
+            <FormControl>
+                <TextField
+                    value={input}
+                    onChange={event => setInput(event.target.value)}
+                    placeholder="enter item"
+                    id="outlined-basic"
+                    label="Add Item"
+                    variant="outlined"/>
+            </FormControl>
+            <Button disabled={!input} variant="contained" color="primary" onClick={addTodo}>Add</Button>
+
+
+            <ul>
+                <li>Read the bible</li>
+                <li>{todos.map(todo => (
+                    <li>{todo}</li>
+                ))}</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
