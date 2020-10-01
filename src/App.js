a27d414142e947fc3b009ca2c0ee451f6d8652fe
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-10-01 12:10:54 +0200

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,45 +1,57 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
 import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import TodoListComponent from "./TodoListComponent";
 
 function App() {
-//declare constants
-    const [todos, setTodos] = useState(['Read the bible', 'Sweep the room', 'Go to the market', 'Exercise']);
-    const [input, setInput] = useState('');
+  //declare constants
+  const [todos, setTodos] = useState([
+    "Read the bible",
+    "Sweep the room",
+    "Go to the market",
+    "Exercise",
+  ]);
+  const [input, setInput] = useState("");
 
-//submit method
-    const addTodo = (event) => {
-        //fire up logic here
-        //prevent refresh after submit
-        event.preventDefault()
-        setTodos([...todos, input])//add new input value to array
-        setInput('')//reset form to empty
-    }
+  //submit method
+  const addTodo = (event) => {
+    //fire up logic here
+    //prevent refresh after submit
+    event.preventDefault();
+    setTodos([...todos, input]); //add new input value to array
+    setInput(""); //reset form to empty
+  };
 
-    return (
-        <div className="App">
-            <FormControl>
-                <TextField
-                    value={input}
-                    onChange={event => setInput(event.target.value)}
-                    placeholder="enter item"
-                    id="outlined-basic"
-                    label="Add Item"
-                    variant="outlined"/>
-            </FormControl>
-            <Button disabled={!input} variant="contained" color="primary" onClick={addTodo}>Add</Button>
+  return (
+    <div className="App">
+      <FormControl>
+        <InputLabel>Write A To-Do</InputLabel>
+        <Input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          placeholder="enter item"
+        />
+      </FormControl>
+      <Button
+        disabled={!input}
+        variant="contained"
+        color="primary"
+        onClick={addTodo}
+      >
+        Add
+      </Button>
 
-
-            <ul>
-                <li>Read the bible</li>
-                <li>{todos.map(todo => (
-                    <li>{todo}</li>
-                ))}</li>
-            </ul>
-        </div>
-    );
+      <List>
+        {todos.map((todo) => (
+          <TodoListComponent todoItem={todo} />
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/TodoListComponent.js
@@ -0,0 +1,22 @@
+//shortcut if youre using vscode + ES7 snippets ext is 'rfce', and will create your component defs
+
+import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import KeyboardArrowRightRoundedIcon from '@material-ui/icons/KeyboardArrowRightRounded';
+
+
+function TodoListComponent(props) {
+  return (
+    <div>
+      <ListItem>        <ListItemIcon>
+          <KeyboardArrowRightRoundedIcon />
+      </ListItemIcon>
+          <ListItemText primary={props.todoItem} />
+          </ListItem>
+    </div>
+  );
+}
+
+export default TodoListComponent;
